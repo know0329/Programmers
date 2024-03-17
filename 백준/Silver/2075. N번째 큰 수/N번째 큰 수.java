c57ff62a18ac01d368b3db10a9d2ff92eb43ef0a
I-MO <49915643+know0329@users.noreply.github.com>
2024-03-17 22:13:29 +0900

    [Silver II] Title: N번째 큰 수, Time: 808 ms, Memory: 295924 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,16 +11,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(stk.nextToken());
-        int[] arr = new int[N*N];
-        int idx =0;
-        for(int i=0; i < N; i++) {
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+
+        stk = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            que.add(Integer.parseInt(stk.nextToken()));
+        } // 처음 N개 넣기
+
+        for(int i = 1; i < N; i++) {
             stk = new StringTokenizer(br.readLine());
             for(int j=0; j < N; j++) {
-                arr[idx++] = Integer.parseInt(stk.nextToken());
+               int num = Integer.parseInt(stk.nextToken());
+
+               if(que.peek() < num){
+                   que.poll();
+                   que.add(num);
+               }
             }
-        }
-        Arrays.sort(arr);
-        System.out.println(arr[N*N - N]);
+        } // N-1 개 작은 거 부터 순차적으로 빼기 => 다 빼면 peek는 N번쨰 큰 수가 남음
+
+        System.out.println(que.peek());
 
     }
 }
